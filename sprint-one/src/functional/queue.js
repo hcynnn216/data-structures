f5c2990709a35697f9ace9f0fbbf84cfdef7eafa
hcynnn216 <yuhongchao@gmail.com>
2016-06-08 15:07:59 -0700

    Return correct number for size after adding and removing elements

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,22 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var size = 0;
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    size++;
   };
 
   someInstance.dequeue = function() {
+    size--;
+    if (size < 0) {
+      size = 0;
+    }
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
