20f65377a18e10134cf3e1f060c33789b1b1d813
hcynnn216 <yuhongchao@gmail.com>
2016-06-08 16:43:10 -0700

    added objects in stack.js

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,26 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var stack = {};
+  stack.size = 0;
+  stack.storage = {};
+  _.extend(stack, stackMethods);
+  return stack;
 };
 
-var stackMethods = {};
+stackMethods = {};
+stackMethods.push = function(value) {
+  this.size++;
+  this.storage[this.size] = value;
+};
 
+stackMethods.pop = function() {
+  this.size --;
+  var result = this.storage[this.size];
+  delete this.storage[this.size];
+  return result;
+};
 
+stackMethods.size = function() {
+  return this.size;
+};
