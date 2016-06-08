30b392b0beda55cecf58d0a11c04e7fdfb57167c
Frances Yang <franyang99@gamil.com>
2016-06-08 12:05:45 -0700

    Add and remove items from stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -7,11 +7,14 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[size + 1] = value;
     size++;
   };
 
   someInstance.pop = function() {
+    var result = storage[size];
     size = Math.max(size - 1, 0);
+    return result;
   };
 
   someInstance.size = function() {
