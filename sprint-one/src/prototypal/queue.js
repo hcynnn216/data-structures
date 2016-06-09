d4c16591e8ec10a5a64f5bd31245918a4e2ab672
hcynnn216 <yuhongchao@gmail.com>
2016-06-08 20:45:50 -0700

    Create queue using Object.create

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,25 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var queue = Object.create(queueMethods);
+  queue.start = 0;
+  queue.end = 0;
+  queue.storage = {};
+  return queue;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {
+  this.storage[this.end] = value;
+  this.end++;
+};
 
+queueMethods.dequeue = function() {
+  var result = this.storage[this.start];
+  delete this.storage[this.start];
+  this.start = Math.min(this.start + 1, this.end);
+  return result;
+};
+
+queueMethods.size = function() {
+  return this.end - this.start;
+};
