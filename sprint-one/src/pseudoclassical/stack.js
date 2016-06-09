5ab27bf992f9cd7c77b2bb8bcbc002508d00ae2b
hcynnn216 <yuhongchao@gmail.com>
2016-06-08 22:27:13 -0700

    Create stack using new

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,20 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.length = 0;
+  this.storage = {};
 };
 
+Stack.prototype.push = function(value) {
+  this.length++;
+  this.storage[this.length] = value;
+};
 
+Stack.prototype.pop = function() { 
+  var result = this.storage[this.length];
+  delete this.storage[this.length];
+  this.length = Math.max(this.length - 1, 0);
+  return result;
+};
+
+Stack.prototype.size = function() {
+  return this.length;
+};
