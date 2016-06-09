bee296c90e1ea06b2386cb1d3cb72a138f56eb0e
hcynnn216 <yuhongchao@gmail.com>
2016-06-08 20:39:21 -0700

    Create stack using prototyple method

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,23 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var stack = Object.create(stackMethods);
+  stack.length = 0;
+  stack.storage = {};
+  return stack;
 };
 
 var stackMethods = {};
+stackMethods.push = function(value) {
+  this.length++;
+  this.storage[this.length] = value;
+};
 
+stackMethods.pop = function() { 
+  var result = this.storage[this.length];
+  delete this.storage[this.length];
+  this.length = Math.max(this.length - 1, 0);
+  return result;
+};
 
+stackMethods.size = function() {
+  return this.length;
+};
