2bb25b8e251772f9eeb71d6074ec44efdfc6b577
hcynnn216 <yuhongchao@gmail.com>
2016-06-10 11:57:34 -0700

    Tried solving collision

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,5 +1,64 @@
 
 
+/*
+ ********** NOTE: **********
+ * Do not edit this code unless you see a bug!
+ */
+
+
+// This class represents an array with limited functionality and a maximum size.
+// It will ensure that you don't accidentally try to use up too much space.
+//
+// Usage:
+//   limitedArray.set(3, 'hi');
+//   limitedArray.get(3); // returns 'hi'
+
+var LimitedArray = function(limit) {
+  var storage = [];
+
+  var limitedArray = {};
+  limitedArray.get = function(index) {
+    checkLimit(index);
+    return storage[index];
+  };
+  limitedArray.set = function(index, value) {
+    checkLimit(index);
+    storage[index] = value;
+  };
+  limitedArray.each = function(callback) {
+    for (var i = 0; i < storage.length; i++) {
+      callback(storage[i], i, storage);
+    }
+  };
+
+  var checkLimit = function(index) {
+    if (typeof index !== 'number') {
+      throw new Error('setter requires a numeric index for its first argument');
+    }
+    if (limit <= index) {
+      throw new Error('Error trying to access an over-the-limit index');
+    }
+  };
+
+  return limitedArray;
+};
+
+// This is a "hashing function". You don't need to worry about it, just use it
+// to turn any string into an integer that is well-distributed between the
+// numbers 0 and `max`
+var getIndexBelowMaxForKey = function(str, max) {
+  var hash = 0;
+  for (var i = 0; i < str.length; i++) {
+    hash = (hash << 5) + hash + str.charCodeAt(i);
+    hash = hash & hash; // Convert to 32bit integer
+    hash = Math.abs(hash);
+  }
+  return hash % max;
+};
+
+/*
+ * Complexity: What is the time complexity of the above functions?
+ */
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
@@ -7,17 +66,38 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage[index] || [];
+  bucket.push([k, v]);
+  this._storage.set(index, bucket);
+  console.log(bucket);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+  for (var i = 0; i < bucket.length; i++) {
+    if (bucket[i][0] === k)
+      return bucket[i][1];
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+  for (var i = 0; i < bucket.length; i++) {
+    if (bucket[i][0] === k)
+      bucket[i][0] = undefined;
+      bucket[i][1] = undefined;
+  }
+
 };
 
-
+var v1 = 'val1';
+var v2 = 'val2';
+hashTable.insert(v1, v1);
+hashTable.insert(v2, v2);
+console.log(hashTable.retrieve(v1));
+console.log(hashTable.retrieve(v2));
 
 /*
  * Complexity: What is the time complexity of the above functions?
