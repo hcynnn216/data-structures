df145fb25b273211566ad48876592dc626b73cc2
hcynnn216 <yuhongchao@gmail.com>
2016-06-10 16:39:23 -0700

    HashTable implementation

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -13,48 +13,6 @@
 //   limitedArray.set(3, 'hi');
 //   limitedArray.get(3); // returns 'hi'
 
-var LimitedArray = function(limit) {
-  var storage = [];
-
-  var limitedArray = {};
-  limitedArray.get = function(index) {
-    checkLimit(index);
-    return storage[index];
-  };
-  limitedArray.set = function(index, value) {
-    checkLimit(index);
-    storage[index] = value;
-  };
-  limitedArray.each = function(callback) {
-    for (var i = 0; i < storage.length; i++) {
-      callback(storage[i], i, storage);
-    }
-  };
-
-  var checkLimit = function(index) {
-    if (typeof index !== 'number') {
-      throw new Error('setter requires a numeric index for its first argument');
-    }
-    if (limit <= index) {
-      throw new Error('Error trying to access an over-the-limit index');
-    }
-  };
-
-  return limitedArray;
-};
-
-// This is a "hashing function". You don't need to worry about it, just use it
-// to turn any string into an integer that is well-distributed between the
-// numbers 0 and `max`
-var getIndexBelowMaxForKey = function(str, max) {
-  var hash = 0;
-  for (var i = 0; i < str.length; i++) {
-    hash = (hash << 5) + hash + str.charCodeAt(i);
-    hash = hash & hash; // Convert to 32bit integer
-    hash = Math.abs(hash);
-  }
-  return hash % max;
-};
 
 /*
  * Complexity: What is the time complexity of the above functions?
@@ -62,42 +20,84 @@ var getIndexBelowMaxForKey = function(str, max) {
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this._items = 0;
 };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  var bucket = this._storage[index] || [];
-  bucket.push([k, v]);
+  var bucket = this._storage.get(index) || [];
+  var flag = false;   
+  for (var i = 0; i < bucket.length; i++) {
+    if (bucket[i][0] === k) {
+      flag = true;
+      bucket[i][1] = v;
+    }
+  }
+  if (!flag) {
+    bucket.push([k, v]);
+    this._items++;
+  }
   this._storage.set(index, bucket);
-  console.log(bucket);
+  if (this._items / this._limit > .75) {
+    this.changeSize(this._limit + this._limit);
+  } else if (this._items / this._limit < .25) {
+    this.changeSize(Math.ceil(this._limit / 2));
+  } 
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  var bucket = this._storage.get(index);
+  var bucket = this._storage.get(index) || [];
   for (var i = 0; i < bucket.length; i++) {
-    if (bucket[i][0] === k)
+    if (bucket[i][0] === k) {
       return bucket[i][1];
+    }
   }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  var bucket = this._storage.get(index);
+  var bucket = this._storage.get(index) || [];
   for (var i = 0; i < bucket.length; i++) {
-    if (bucket[i][0] === k)
-      bucket[i][0] = undefined;
-      bucket[i][1] = undefined;
+    if (bucket[i][0] === k) {
+      bucket.splice(i, 1);
+      this._items--;
+    }
   }
-
+  if (this._items / this._limit > .75) {
+    this.changeSize(this._limit + this._limit);
+  } else if (this._items / this._limit < .25) {
+    this.changeSize(Math.ceil(this._limit / 2));
+  }
 };
 
-var v1 = 'val1';
-var v2 = 'val2';
-hashTable.insert(v1, v1);
-hashTable.insert(v2, v2);
-console.log(hashTable.retrieve(v1));
-console.log(hashTable.retrieve(v2));
+HashTable.prototype.changeSize = function(newSize) {
+  var oldStorage = this._storage;
+  var oldLimit = this._limit;
+  this._limit = newSize;
+  this._storage = new LimitedArray(this._limit);
+  for (var i = 0; i < oldLimit; i++) {
+    var oldBucket = oldStorage.get(i) || [];
+    for (var j = 0; j < oldBucket.length; j++) {
+      // repeat of insert method
+      var key = oldBucket[j][0];
+      var value = oldBucket[j][1];
+      var index = getIndexBelowMaxForKey(key, this._limit);
+      var bucket = this._storage.get(index) || [];
+      var flag = false;   
+      for (var l = 0; l < bucket.length; l++) {
+        if (bucket[l][0] === key) {
+          flag = true;
+          bucket[l][1] = key;
+        }
+      }
+      if (!flag) {
+        bucket.push([key, value]);
+      }
+      this._storage.set(index, bucket);
+    }
+  }
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
